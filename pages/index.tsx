efaef3e9b9ac62316252fd9fbed8c276e1ebe83d
mortenhillbom <morten.hillbom@cognite.com>
2020-10-03 18:04:30 +0200

    Make layout resizable

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import QueryEditor from "components/QueryEditor";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import styles from "../styles/Home.module.css";
+import { Resizable } from "re-resizable";
 
 type ResultRow = { [key: string]: string | number | any };
 
@@ -62,7 +62,7 @@ export default function Home(): JSX.Element {
   }, [resultRows]);
 
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>SQL front end</title>
         <link rel="icon" href="/favicon.ico" />
@@ -73,30 +73,59 @@ export default function Home(): JSX.Element {
           Reset DB
         </Button>
       </TitleWrapper>
-      <QueryWrapper>
-        <Title level={3}>Query:</Title>
-        <QueryEditor query={userQuery} setQuery={setUserQuery} />
-        <Button onClick={handleRunQuery} type="primary">
-          Run query
-        </Button>
-      </QueryWrapper>
-      <ResultWrapper>
-        <Title level={3}>Results:</Title>
-        {errorMessage && <Alert type="error" message={errorMessage} />}
-        {columns.length ? (
-          <Table<ResultRow>
-            scroll={{ x: "auto", y: "50vh" }}
-            columns={columns}
-            dataSource={resultRows}
-          />
-        ) : (
-          <Empty style={{ margin: 40 }} />
-        )}
-      </ResultWrapper>
-    </div>
+      <Content>
+        <QueryWrapper>
+          <QueryEditor query={userQuery} setQuery={setUserQuery} />
+          <Button onClick={handleRunQuery} type="primary" size="large">
+            Run query
+          </Button>
+        </QueryWrapper>
+        <Resizable
+          defaultSize={{
+            width: "60%",
+            height: "100%",
+          }}
+          maxWidth="98vw"
+          minWidth="12"
+          enable={{
+            top: false,
+            right: false,
+            bottom: false,
+            left: true,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          style={{ display: "flex" }}
+        >
+          <ResizeBorder>||</ResizeBorder>
+          <ResultWrapper>
+            {errorMessage && <Alert type="error" message={errorMessage} />}
+            {columns.length ? (
+              <Table<ResultRow>
+                scroll={{ x: "auto", y: "auto" }}
+                columns={columns}
+                dataSource={resultRows}
+              />
+            ) : (
+              <Empty description="No query results" style={{ margin: 40 }} />
+            )}
+          </ResultWrapper>
+        </Resizable>
+      </Content>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: left;
+`;
 const TitleWrapper = styled.div`
   padding: 16px;
   display: flex;
@@ -107,12 +136,34 @@ const TitleWrapper = styled.div`
 const QueryWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: 20vh;
-  margin-bottom: 24px;
+  height: 100%;
+  width: 100%;
 `;
 
 const ResultWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: 50vh;
+  padding-bottom: 8px;
+  width: 100%;
+  overflow: scroll;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  border-top: 5px solid #efefef;
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 97px);
+`;
+
+const ResizeBorder = styled.div`
+  width: 12px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: -1px;
+  color: black;
+  background: #efefef;
+  user-select: none;
 `;
